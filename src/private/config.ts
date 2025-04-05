770de7ebeafd94fe4432a95662e478116843b685
Dang Tu <anhtuhanam1@gmail.com>
2025-04-06 03:00:58 +0700

    update

--- src/private/config.ts
+++ src/private/config.ts
@@ -44,29 +44,29 @@ const tdsAccounts: TDSAccount[] = [
       },
     ],
   },
-  {
-    token: 'TDS0nIxIXZ2V2ciojIyVmdlNnIsISMxEjM1R3ZuFGZiojIyV2c1Jye',
-    username: 'dangtu2111',
-    password: 'tu211102',
-    facebook: [
-      {
-        id: '61573445621892 ',
-        name: 'Phạm Nguyễn',
-        email: '61573445621892',
-        password: 'leajax631z',
-        cookie:'datr=9G3xZ-NDre_Ymd5xmWP1B9vY; sb=9G3xZ_DNXX_cpN6g_PspQx6N; dpr=1.375; c_user=61573445621892; xs=29%3AWA6feqa9eWq54w%3A2%3A1743875584%3A-1%3A12252; fr=08jNNho7cfCI4a3Zs.AWfAf3G8DuCt9wZfD7o4w0mXCbTLkaluwAxG0OOgvnW2EQVV-zc.Bn8W30..AAA.0.0.Bn8W4B.AWdIHEdp7MPM58fUPD2Ld1qnWzU; ps_l=1; ps_n=1; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1743875587542%2C%22v%22%3A1%7D; wd=693x632',
+  // {
+  //   token: 'TDS0nIxIXZ2V2ciojIyVmdlNnIsISMxEjM1R3ZuFGZiojIyV2c1Jye',
+  //   username: 'dangtu2111',
+  //   password: 'tu211102',
+  //   facebook: [
+  //     {
+  //       id: '61573445621892 ',
+  //       name: 'Phạm Nguyễn',
+  //       email: '61573445621892',
+  //       password: 'leajax631z',
+  //       cookie:'datr=9G3xZ-NDre_Ymd5xmWP1B9vY; sb=9G3xZ_DNXX_cpN6g_PspQx6N; dpr=1.375; c_user=61573445621892; xs=29%3AWA6feqa9eWq54w%3A2%3A1743875584%3A-1%3A12252; fr=08jNNho7cfCI4a3Zs.AWfAf3G8DuCt9wZfD7o4w0mXCbTLkaluwAxG0OOgvnW2EQVV-zc.Bn8W30..AAA.0.0.Bn8W4B.AWdIHEdp7MPM58fUPD2Ld1qnWzU; ps_l=1; ps_n=1; presence=C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1743875587542%2C%22v%22%3A1%7D; wd=693x632',
           
-        },
-      {
-        id: '61559203291191',
-        name: 'phan_huyen_trang',
-        email: '61559203291191',
-        password: 'tu211102',
-        cookie:
-          'datr=AsdRZ7Klhu3oEssMkK4UKwky; sb=AsdRZ2sLZaqhYe57c26iupO6; dpr=1.25; wd=978x738; ps_l=1; ps_n=1; locale=vi_VN; c_user=61559203291191; xs=12%3ArCsPUqhktUN2ew%3A2%3A1733413596%3A-1%3A6278; fr=0boSv9rQy9Xu7jcsg.AWWR1_3T3vv5x9aFtN7AWCLUeU0.BnUccC..AAA.0.0.BnUcre.AWWZSAXMbuA',
-      },
-    ],
-  },
+  //       },
+  //     {
+  //       id: '61559203291191',
+  //       name: 'phan_huyen_trang',
+  //       email: '61559203291191',
+  //       password: 'tu211102',
+  //       cookie:
+  //         'datr=AsdRZ7Klhu3oEssMkK4UKwky; sb=AsdRZ2sLZaqhYe57c26iupO6; dpr=1.25; wd=978x738; ps_l=1; ps_n=1; locale=vi_VN; c_user=61559203291191; xs=12%3ArCsPUqhktUN2ew%3A2%3A1733413596%3A-1%3A6278; fr=0boSv9rQy9Xu7jcsg.AWWR1_3T3vv5x9aFtN7AWCLUeU0.BnUccC..AAA.0.0.BnUcre.AWWZSAXMbuA',
+  //     },
+  //   ],
+  // },
 ];
 
 // Xuất dữ liệu
